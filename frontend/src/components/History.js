11d9e35234b3e8de51d737d00305868fc1070924
OscarSantos1 <ossantos1994@gmail.com>
2023-03-28 04:50:09 -0600

    history style updated

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -1,65 +1,71 @@
-import { useState, useEffect, useContext } from 'react'
-import { UserContext } from './UserContext'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "./UserContext";
+import NavBar from "./NavBar";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const History = () => {
-  const [ history, setHistory ] = useState([])
-  const monthsAbbrev = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dic']
-  const { APP_API_URL } = useContext(UserContext)
+  const [history, setHistory] = useState([]);
+  const monthsAbbrev = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dic",
+  ];
+  const { APP_API_URL } = useContext(UserContext);
 
-  useEffect(()=>{
+  useEffect(() => {
     const queryHistory = async () => {
-      let head = { headers: {'Authorization': `Bearer ${sessionStorage.getItem('doMeToken')}` }}
-      let payload = { 'type': 'histo', 'userId': sessionStorage.getItem('id') }
-      const { data } = await axios.post(`${APP_API_URL}/tasks`, payload, head)
-      setHistory(data.history)
-      console.log('here is the history')
-      console.log(data.history)
-    }
+      let head = {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("doMeToken")}`,
+        },
+      };
+      let payload = { type: "histo", userId: sessionStorage.getItem("id") };
+      const { data } = await axios.post(`${APP_API_URL}/tasks`, payload, head);
+      setHistory(data.history);
+      console.log("here is the history");
+      console.log(data.history);
+    };
 
-    queryHistory()
-
-  }, [APP_API_URL])
-
+    queryHistory();
+  }, [APP_API_URL]);
 
   const historyListItems = history.map((item, index) => {
     return (
-      <li className='history-task' key={index}>
-        <div className='history-task-flex' >
-          <div className='circle-container' >
-            <div className='bullet'>
-              +
-            </div>
+      <li className="history-task" key={index}>
+        <div className="history-task-flex">
+          <div className="circle-container">
+            <div className="bullet">+</div>
           </div>
-          <div className='info-container'>
-            <div className='description-container'>
-              {item.description}
-            </div>
-            <div className='date-container'>
-              {item.day + ' ' + monthsAbbrev[item.month - 1] + ' ' + item.year}
+          <div className="info-container">
+            <div className="description-container">{item.description}</div>
+            <div className="date-container">
+              {item.day + " " + monthsAbbrev[item.month - 1] + " " + item.year}
             </div>
           </div>
         </div>
       </li>
-    )
-  })
+    );
+  });
 
   return (
-    <div>
-      <div className="app-name-bar"><Link className="app-name" to="/" ><div >Do me.</div></Link></div>
-      <div className='cont-container'>
-        <div className='container cont-padding-right overflow'>
-          <ul className='history-list'>
-            {historyListItems}
-          </ul>
-        </div>
+    <div className="flex flex-col justify-between items-center space-y-4 md:space-y-9 2xl:space-y-24 h-screen w-screen bg-[#F1F2F7]">
+      <NavBar />
+      <div className="h-[70%] md:w-[28.5%] w-[90%] overflow-scroll rounded-[20px] bg-[#F1F2F7]">
+        <ul className="history-list">{historyListItems}</ul>
       </div>
-      <footer>
-        <p className='align'><Link to="/">Go to Home</Link></p>
-      </footer>
+      <div />
     </div>
-  )
-}
+  );
+};
 
-export default History
+export default History;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -16,19 +16,19 @@ const NavBar = () => {
     navigate("/");
   };
   return (
-    <Link className="app-name" to="/">
-      <div className="flex justify-between items-center px-2 md:px-6 py-2 border-b border-[#d4d3d3] w-full">
+    <div className="flex justify-between items-center px-2 md:px-6 py-2 border-b border-[#d4d3d3] w-full">
+      <Link className="app-name" to="/">
         <div className="text-slate-100 text-xl md:text-3xl font-normal bg-[#8F9C2F] pt-[6px] pb-[7px] px-6 rounded-[16px]">
           Diurnal
         </div>
-        <div>
-          <Link to="/history">History</Link>
-          <button className="log-out-btn" onClick={logOut}>
-            Log Out
-          </button>
-        </div>
+      </Link>
+      <div>
+        <Link to="/history">History</Link>
+        <button className="log-out-btn" onClick={logOut}>
+          Log Out
+        </button>
       </div>
-    </Link>
+    </div>
   );
 };
 
